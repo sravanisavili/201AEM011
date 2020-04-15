21a315bb228fc16b77f061cf9e189230be66e864
sravanisavili <63168257+sravanisavili@users.noreply.github.com>
2020-04-15 23:43:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-from flask import Flask
-from flask_restful import Resource,  Api
+from flask import Flask, request #importing request library
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-items = []
+items = [] #creating a list called items (contains a dictionary for each item)
+
+class Item(Resource): #Let us rename our Resource as Item
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404 #setting up an error code if requested object cannot be found
+
+    def post(self, name):
+            data = request.get_json()
+            item = {'name': name, 'price': data['price']} # will access the price key of a data dictionary
+            items.append(item)
+            return item, 201 #status code showing a successful creating of an object
 
 
-class Item(Resource):
-     def get(self, name):
-         for item in items:
-             if item['name'] == name:
-                return name
-         return  {'item' : None}, 404
+class ItemList(Resource): # Our new Resource
+    def get(self):
+        return {'items': items} #returns a list of items (dictionary items)
 
-     def post(self, name):
-              item = {'name' : name, 'price' : 10.00}
-              items.append(item)
-              return item, 201
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items') #add a resource with a correct endpoint
 
-api.add_resource(Item, '/item//<string:name>')
-
-
-app.run(port=5000)
-  #sravani
+app.run(port=5000, debug=True) # Makes an error (if smth goes wrong) more clear in form of html
