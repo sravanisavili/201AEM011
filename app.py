ebc4a55ac0837d5222ade118e5e1385ed68ac792
sravanisavili <63168257+sravanisavili@users.noreply.github.com>
2020-05-17 19:58:37 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,24 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+
+from flask_restful import Resource, Api,  reqparse
+from flask_jwt import JWT, jwt_required, current_identity
 
 from security import authenticate, identity
+from user import UserRegister
+from item import Item
+
 
 app = Flask(__name__)
-app.secret_key = 'sravani'
+
+app.secret_key = 'Girija'
+
 api = Api(app)
-
-items = []
-
-class Item(Resource):
-    def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item' : None}, 404
-
-    def post(self, name):
-        data = request.get_json()
-        item = {'name' : name, 'price' : data['price']}
-        items.append(item)
-        return item, 201
-    def put(self, name):
-        parser =  reqparse.RequestParser()
-        parser.add_argument('price',
-            type = float,
-            required = True,
-            help = 'This field cannot be left blank!'
-        )
-
-class Itemlist(Resource):
-    def get(self):
-        return {'items' : items}
-
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(Itemlist, '/items')
 
 jwt = JWT(app, authenticate, identity)
 
-@app.route('/auth')
-@jwt_required()
-def auth():
-    return '%s' % current_identity
+api.add_resource(Item, '/<name>')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
     #sravani
---
+++ item.py
@@ -0,0 +1,94 @@
+from flask_restful import Resource, Api,  reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+import sqlite3
+
+
+class Item(Resource):
+    TABLE_NAME = 'items'
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float,required=True, help='This field cannot be left blank')
+
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message':'Item not found'}, 404
+
+
+    @jwt_required()
+    def post(self, name):
+        if self.find_by_name(name):
+            return {"message": "An item with name '{}' already exists".format(name)}
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price':data['price']}
+        try:
+            Item.insert(item)
+        except:
+            return {"message":"An error occured during inserting item"}, 500
+        return item, 201
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = self.find_by_name(name)
+        update_item = {'name': name, 'price':data['price']}
+        if item is None:
+            try:
+                Item.insert(update_item)
+            except:
+                return {"message":"an error occured during update an item"}
+        else:
+            try:
+                Item.update(update_item)
+            except:
+                raise
+                return {"message":"an error occured during update an item"}
+        return update_item
+
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM {table} WHERE name=?".format(table=cls.TABLE_NAME)
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+        if row:
+            return {'item': {'name':row[0], 'price':row[1]}}
+
+
+
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO {table} VALUES (?,?)".format(table=cls.TABLE_NAME)
+        cursor.execute(query, (item['name'],item['price']))
+        connection.commit()
+        connection.close()
+        return {'message':'Item Added'}
+
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "UPDATE {table} SET price=? WHERE name=?".format(table=cls.TABLE_NAME)
+        cursor.execute(query, (item['price'],item['name']))
+        connection.commit()
+        connection.close()
+        return {'message':'Item Updated'}
+
+    @jwt_required()
+    def delete(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "DELETE FROM {table} WHERE name=?".format(table=cls.TABLE_NAME)
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
+        return {'message':'Item Deleted'}
